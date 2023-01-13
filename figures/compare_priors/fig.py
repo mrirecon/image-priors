9663ce8352ef07ca7b929f4ef31b3502c0b9f8ab
Guanxiong Luo <guanxiong.luo@med.uni-goettingen.de>
2023-01-13 18:31:21 +0100

    update compare_priors

--- figures/compare_priors/fig.py
+++ figures/compare_priors/fig.py
@@ -11,7 +11,8 @@ recos       = ['zero_filled', 'l1_pics', 'l1_nlinv', 'prior_abide_pics',
 
 normalize = lambda x: abs(x)/np.linalg.norm(abs(x))
 readcfl   = lambda dir, file: utils.readcfl(os.path.join(dir, file))
-
+win_ftr   = 0.65
+saveimg   = lambda x, upper, name: utils.save_img(x, "./sub_figs/"+name, vmax=upper*win_ftr)
 
 und_kspace = readcfl(os.path.join(result_path, exprs[0]), 'und_kspace')
 coilsen = abs(utils.bart(1, 'ecalib -r 20 -m1', und_kspace))
@@ -32,11 +33,15 @@ for expr in exprs:
 
     cur_dir     = os.path.join(result_path, expr)
     grd         = normalize(readcfl(cur_dir, 'grd'))
+    upper       = np.max(grd)
+    saveimg(grd, upper, 'grd')
+
     print('%s\t&\t'%expr, end='')
     for i, reco in enumerate(recos):
 
         img = normalize(readcfl(cur_dir, reco))
         psnr = utils.psnr(grd, img*mask)
+        saveimg(img, upper, expr+'_'+reco)
         print("%.2f"%psnr, end='')
 
         if i != len(recos)-1:
