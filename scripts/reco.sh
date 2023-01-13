009a4be30e672377216742e83745d124ba57cfad
Guanxiong Luo <guanxiong.luo@med.uni-goettingen.de>
2023-01-13 17:56:27 +0100

    updated reco.sh

--- scripts/reco.sh
+++ scripts/reco.sh
@@ -27,17 +27,17 @@ read -p 'Select undersampling pattern.
          po is poisson disc [1d]: ' PATTERN
 PATTERN=${PATTERN:-1d}
 
-GRAPH=$ROOT_PATH/logs/exported/pixelcnn_abide
+GRAPH1=$ROOT_PATH/logs/exported/pixelcnn_abide
+GRAPH2=$ROOT_PATH/logs/exported/pixelcnn_abide_filtered
+GRAPH3=$ROOT_PATH/logs/exported/pixelcnn_hku
+
 DATA_PATH=$ROOT_PATH/data/kspaces/mprage
-#GRAPH=/home/gluo/workspace/nlinv/logs/exported/pixelcnn_hku
-#GRAPH=/home/gluo/workspace/nlinv/logs/exported/pixelcnn_abide_filtered
-#DATA_PATH=/home/gluo/workspace/nlinv/scripts/data/kspace
 
 mkdir -p $ROOT_PATH/results/$PATTERN
 cd $ROOT_PATH/results/$PATTERN
 
 if [ $PATTERN == po ]; then
-    bart poisson -Y$nx -Z$ny -y1.5 -z1.5 -v -s 1000 -C$cal mask
+    bart poisson -Y$nx -Z$ny -y1.5 -z1.5 -v -s 1000 -C$(($cal*2)) mask
     bart transpose 0 1 mask mask
     bart transpose 1 2 mask mask
 fi
@@ -61,9 +61,14 @@ bart fmac -C -s$(bart bitmask 3) coilimgs coilsen grd
 bart fft -i 3 und_kspace coilimgs
 bart fmac -C -s$(bart bitmask 3) coilimgs coilsen zero_filled
 
+bart pics -g -l2 -r 0.01 und_kspace coilsen l2_pics
 bart pics -g -l1 -r 0.01 und_kspace coilsen l1_pics
-bart pics -g -i100 -d4 -R TF:{$GRAPH}:0.8 und_kspace coilsen prior_pics
+#bart pics -g -i100 -d4 -R TF:{$GRAPH1}:0.8 und_kspace coilsen prior_abide_pics
+#bart pics -g -i100 -d4 -R TF:{$GRAPH2}:0.8 und_kspace coilsen prior_abide_filtered_pics
+#bart pics -g -i100 -d4 -R TF:{$GRAPH3}:0.8 und_kspace coilsen prior_hku_pics
 
 bart nlinv -g -d4 -a660 -b44 -i14 -C50 und_kspace nlinv nlinv_coils
 bart nlinv -g -d4 -a660 -b44 -i15 -C50 --reg-iter=5 -R W:3:0:0.1 und_kspace l1_nlinv l1_nlinv_coils
-bart nlinv -g -d4 -a660 -b44 -i14 -C50 --reg-iter=5 -R LP:{$GRAPH}:0.8:1:1 und_kspace prior_nlinv prior_nlinv_coils
+#bart nlinv -g -d4 -a660 -b44 -i14 -C50 --reg-iter=5 -R LP:{$GRAPH1}:0.8:1 und_kspace prior_abide_nlinv prior_abide_nlinv_coils
+#bart nlinv -g -d4 -a660 -b44 -i14 -C50 --reg-iter=5 -R LP:{$GRAPH2}:0.8:1 und_kspace prior_abide_filtered_nlinv prior_abide_filtered_nlinv_coils
+#bart nlinv -g -d4 -a660 -b44 -i14 -C50 --reg-iter=5 -R LP:{$GRAPH3}:0.8:1 und_kspace prior_hku_nlinv prior_hku_nlinv_coils
