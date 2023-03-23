56fa51eb20b194f48a0e3dd40e34ee6bf6518eb1
Luo, Guanxiong <guanxiong.luo@med.uni-goettingen.de>
2023-03-23 14:04:52 +0100

    updated figure for phase_augment

---
+++ figures/phase_augmentation/sub_figs/crop.sh
@@ -0,0 +1,10 @@
+convert bad__.png -crop 432x432+40+40 bad__crop.png
+convert bad_.png -crop 432x432+40+40 bad_crop.png
+convert good_.png -crop 432x432+40+40 good_crop.png
+convert good__.png -crop 432x432+40+40 good__crop.png
+convert okay_.png -crop 432x432+50+50 okay_crop.png
+convert okay__.png -crop 432x432+50+50 okay__crop.png
+
+convert okay_phase_.png -crop 432x432+50+50 okay_phase_crop.png
+convert good_phase_.png -crop 432x432+40+40 good_phase_crop.png
+convert bad_phase_.png -crop 432x432+40+40 bad_phase_crop.png
