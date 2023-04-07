f5e7b379a31faf3fb47a78df57c67a8c7928c8dc
luo9 <guanxiong.luo@med.uni-goettingen.de>
2023-04-07 15:37:20 +0200

    updated tfl3d.sh

--- scripts/recon/tfl3d.sh
+++ scripts/recon/tfl3d.sh
@@ -20,7 +20,6 @@ fi
 echo "Working in the folder $WORKSPACE"
 cd $WORKSPACE
 
-
 ## load and prepare k-space
 dat=/storage/MRT4/BIGDATA/BIGDATA#SMRT4#F39037#M45#D170323#T103009#UE01_t1_tfl3d_ns_sag_TI900_FA9_.dat
 vcc=64
@@ -33,17 +32,19 @@ if [ ! -f kdat.cfl ] ; then
     bart transpose 0 4 kdat__ kdat_
     '
     #bart cc -p $vcc kdat_ ckdat
+    #bart cc -p $vcc kdat_u ckdat_u
     bart fft -i $(bart bitmask 0 1 2) kdat_ coil_imgs
-    bart ecalib -r 25 -c 0.00001 -m1 kdat_ coils
     bart rss $(bart bitmask 3) coil_imgs rss
-    bart fmac -C -s $(bart bitmask 3) coil_imgs coils coil_comb
-    
+
     bart poisson -Y256 -Z176 -v -y1.2 -z1.2 -C25 mask
     bart fmac mask kdat_ kdat_u
-    #bart cc -p $vcc kdat_u ckdat_u
+    bart ecalib -r 25 -c 0.00001 -m1 kdat_u coils
+    bart fmac -C -s $(bart bitmask 3) coil_imgs coils coil_comb
+    bart pics -g -d4 -l1 -r0.01 -i100 kdat_u coils l1_pics
+  
 fi
 
-EXPR=/home/gluo/workspace/nlinv_prior/scripts/recon/export_tfl3d.py
+EXPR=/home/gluo/workspace/nlinv_prior/scripts/recon/create_graph.py
 total=256
 batches=4
 batch_size=64
@@ -53,7 +54,7 @@ path=/home/gluo/workspace/nlinv_prior/logs/exported/test
 
 export_graph()
 {
-python $EXPR $total $batches $batch_size $1 $log $meta $path $1
+python $EXPR $total $batches $batch_size $log $meta $path $1 $2 $1 TFL3D
 }
 
 #declare -a types=("log" "quad" "linear" "sqrt" "exp")
@@ -61,9 +62,7 @@ declare -a types=("log" "quad" "linear")
 
 for type in "${types[@]}"
 do
-    export_graph  $type
+    export_graph  $type SDE
     GRAPH=$path/$type
     bart pics -g -d4 -R DP:{$GRAPH}:0.4:60 -i60 kdat_u coils dp_pics_$type
 done
-
-bart pics -g -d4 -l1 -r0.01 -i100 kdat_u coils l1_pics
