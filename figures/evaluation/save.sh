ae866ce2440d46697d8c3cf6b0c6bba53bddad20
Luo, Guanxiong <guanxiong.luo@med.uni-goettingen.de>
2023-06-20 13:59:49 +0200

    delete the folder figures

--- figures/evaluation/save.sh
+++
@@ -1,5 +0,0 @@
-number=16986
-cp /scratch/gluo/umg_tfl3d/$number/coil_comb.* /scratch/gluo/umg_tfl3d/8x/$number/
-cp /scratch/gluo/umg_tfl3d/$number/l1_pics.* /scratch/gluo/umg_tfl3d/8x/$number/
-cp /scratch/gluo/umg_tfl3d/$number/dp_pics.* /scratch/gluo/umg_tfl3d/8x/$number/
-cp /scratch/gluo/umg_tfl3d/$number/dp_nlinv.* /scratch/gluo/umg_tfl3d/8x/$number/
